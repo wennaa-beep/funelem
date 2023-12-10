db0906024f504bbe3062d57f448ed683558b3831
wennaa-beep <wenabeldeleon6@gmail.com>
2023-12-10 17:47:29 +0800

    second commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'funtasticelemts.app',
+  appName: 'FuntasticElements',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
